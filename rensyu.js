14fef06653beb533ef9b9d51b123e109ee46c57d
matu-mi <emixylo49@gmail.com>
2019-05-25 15:49:15 +0900

    GitHubとJSの練習

---
+++ rensyu.js
@@ -0,0 +1,17 @@
+//5月24日朝
+console.log("ハロー！");
+
+console.log(1+2);
+console.log(2-1);
+console.log(2*3);
+console.log(4/2);
+
+console.log(1+2+3);
+console.log(1+4/2);
+
+console.log(10/(2* -5+12));
+
+let animal="にゃーん";
+console.log(animal);
+animal="ねこ";
+console.log(animal);
